c5bd9aa7330d6c2791b92496597cbb6b25f23c0c
mpsalome <marcuspedro7@gmail.com>
2020-11-07 17:38:41 -0300

    Setted up all routes

---
+++ src/services/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const URL = "http://api.olhovivo.sptrans.com.br/v2.1";
+
+export default {
+  async getBus(searchTerm) {
+    const response = await axios.get(`${URL}/Linha/Buscar?${searchTerm}`);
+    return response.data;
+  },
+  async getBusStop(searchTerm) {
+    const response = await axios.get(
+      `${URL}/Parada/Buscar?termosBusca=${searchTerm}`
+    );
+    return response.data;
+  },
+  async getEstimatedTime(searchTerm) {
+    const response = await axios.get(
+      `${URL}/Previsao?codigoParada=${searchTerm.busstop}&codigoLinha=${searchTerm.bus}`
+    );
+    return response.data;
+  },
+  async postLogin(token) {
+    try {
+      const response = await axios.post(
+        `${URL}/Login/Autenticar?token=${token}`
+      );
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+};

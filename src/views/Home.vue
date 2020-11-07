7eeebb9b23913884dd2134ac4f86d78b9cbad856
mpsalome <marcuspedro7@gmail.com>
2020-11-07 15:14:27 -0300

    Made home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,60 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div class="box__options">
+      <router-link to="/bus">
+        <div class="go-to">
+          <i class="fas fa-bus icon bus"></i>
+          <h2 class="title">Pesquisar Linha</h2>
+        </div>
+      </router-link>
+      <router-link to="/bus-stop">
+        <div class="go-to">
+          <i class="fas fa-map-marker-alt icon bus-stop"></i>
+          <h2 class="title">Pesquisar Parada</h2>
+        </div>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {}
 };
 </script>
+<style lang="scss" scoped>
+.box__options {
+  display: flex;
+  flex-direction: column;
+}
+.go-to {
+  color: #000;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 250px;
+  height: 120px;
+  margin: 0 auto 29px;
+  border: 1px solid #dddd;
+  .icon {
+    font-size: 3em;
+    display: block;
+    margin: 0 auto 8px;
+    width: 3em;
+    height: auto;
+  }
+  .title {
+    font-size: 1.5em;
+  }
+  &:hover {
+    background-color: #ddd;
+    .title,
+    .icon {
+      color: #616161;
+    }
+  }
+}
+</style>

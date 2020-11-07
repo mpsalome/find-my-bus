cc6e59f879e7e05e1c03843617226f0986d82ea6
mpsalome <marcuspedro7@gmail.com>
2020-11-07 15:00:05 -0300

    List item component

---
+++ src/components/ListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="list-item">
+    <i v-if="type === 'bus'" class="fas fa-bus icon bus"></i>
+    <i v-else class="fas fa-map-marker-alt icon bus-stop"></i>
+    <strong>{{ title }}</strong>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: {
+    type: String,
+    title: String,
+    text: String
+  },
+  components: {},
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+.list-item {
+  border-bottom: 1px solid #b0b0b0;
+  height: 70px;
+  padding: 10px;
+  .icon {
+    font-size: 20px;
+    }
+  }
+  p{
+   color: #595959
+  }
+  &:hover {
+    background-color: #b0b0b0;
+  }
+}
+</style>
